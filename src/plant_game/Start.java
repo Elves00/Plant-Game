54f3096452589fadacfca4147de255906b87fddd
Elves00 <91095059+Elves00@users.noreply.github.com>
2021-10-11 13:35:12 +1300

    Dog 2

--- src/plant_game/Start.java
+++ src/plant_game/Start.java
@@ -17,6 +17,7 @@ public class Start {
 
     public static void main(String[] args) {
         try {
+            System.out.println("Dooogg");
             View view = new View();
             Model model = new Model();
             Controller controller = new Controller(model, view);
