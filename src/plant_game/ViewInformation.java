9496bfcc48441b546600aafa768f05b14e89dd84
Elves00 <91095059+Elves00@users.noreply.github.com>
2021-10-19 15:32:48 +1300

    Resolving conflict from merge?

--- src/plant_game/ViewInformation.java
+++ src/plant_game/ViewInformation.java
@@ -1,52 +1,4 @@
-<<<<<<< HEAD
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
-package plant_game;
 
-import java.awt.BorderLayout;
-import javax.swing.JPanel;
-import javax.swing.JScrollPane;
-import javax.swing.JTextArea;
-
-/**
- * A display panel used to present information from the plant game info table.
- *
- * @author breco
- */
-public class ViewInformation extends JPanel {
-
-    private final JScrollPane infoScroller;
-    private final JTextArea infoArea = new JTextArea();
-
-    public ViewInformation() {
-        super();
-        this.setLayout(new BorderLayout());
-        //Disables user from editing text.
-        this.infoArea.setEditable(false);
-        //Set up info scroller
-        this.infoScroller = new JScrollPane(this.infoArea);
-        this.add(this.infoScroller);
-    }
-
-    /**
-     * Updates information displayed on the info area panel.
-     *
-     * @param infoArray
-     */
-    public void updateInformationDisplay(String... infoArray) {
-        String toDisplay = "";
-        for (String infoArray1 : infoArray) {
-            toDisplay += infoArray1 + "\n";
-        }
-        this.infoArea.setText(toDisplay);
-
-    }
-
-}
-=======
 /*
  * To change this license header, choose License Headers in Project Properties.
  * To change this template file, choose Tools | Templates
@@ -96,4 +48,3 @@ public class ViewInformation extends JPanel {
     }
 
 }
->>>>>>> Cleanup
